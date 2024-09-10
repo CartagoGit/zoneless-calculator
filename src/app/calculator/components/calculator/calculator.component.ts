6eddadaf09e2744b1b8d6c94a914439c9ee272ea
Cartago <cartago.relaxingcup@gmail.com>
2024-09-11 00:37:34 +0200

    Creamos diccionario para teclas especiales y traspilarlas a la calculadora

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  viewChildren,
+} from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 @Component({
@@ -8,17 +13,32 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    "(document:keyup)": "handleKeyboardEvent($event)"
-  }
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
 })
 export class CalculatorComponent {
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
   handleClick(key: string) {
     console.log({ key });
   }
 
   handleKeyboardEvent(event: KeyboardEvent) {
     event.preventDefault();
-    const key = event.key;
-    this.handleClick(key);
+    const key = event.key.toLowerCase();
+    const keyDictionary: Record<string, string> = {
+      escape: 'C',
+      clear: 'C',
+      c: 'C',
+      enter: '=',
+      x: '*',
+      ',': '.',
+      '/': '÷',
+    };
+    const keyEquivalent = keyDictionary[key] ?? key;
+    this.handleClick(keyEquivalent);
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyle(keyEquivalent);
+    });
   }
 }
